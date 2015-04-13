5c6e25fd55fdcc9a83af45ad7ccbedfed9271fad
Giau Ngo <giaunv@gmail.com>
2015-04-13 07:27:00 +0700

    Scripts: Adding Force to JetPack

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+	public float jetpackForce = 75.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// FixedUpdate is called at a fixed rate, should be used to write the code related to physics simulation
+	void FixedUpdate(){
+		bool jetpackActive = Input.GetButton ("Fire1");
+		
+		if (jetpackActive) {
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
+		}
+	}
+}
