27ee0254aeedad48b0aa7c523d9f1f04963d762d
Giau Ngo <giaunv@gmail.com>
2015-04-15 06:50:27 +0700

    Generating Endless Level: Adding Script to Generate Room

---
+++ Assets/Scripts/GeneratorScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratorScript : MonoBehaviour {
+	public GameObject[] availableRooms;
+	public List<GameObject> currentRooms;
+	public float screenWidthInPoints;
+
+	// Use this for initialization
+	void Start () {
+		float height = 2.0f + Camera.main.orthographicSize;
+		screenWidthInPoints = height + Camera.main.aspect;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
