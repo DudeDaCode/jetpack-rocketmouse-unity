04d4a6973f138b47ba4b4aee48f1355b1a71a379
Giau Ngo <giaunv@gmail.com>
2015-04-14 23:09:54 +0700

    Making the Mouse Fly Forward: Keeping the Camera at the Distance

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 	public GameObject targetObject;
+	private float distanceTarget;
 
 	// Use this for initialization
 	void Start () {
-	
+		distanceTarget = transform.position.x - targetObject.transform.position.x;
 	}
 	
 	// Update is called once per frame
@@ -14,7 +15,7 @@ public class CameraFollow : MonoBehaviour {
 		float targetObjectX = targetObject.transform.position.x;
 
 		Vector3 newCameraPosition = transform.position;
-		newCameraPosition.x = targetObjectX;
+		newCameraPosition.x = targetObjectX + distanceTarget;
 		transform.position = newCameraPosition;
 	}
 }
