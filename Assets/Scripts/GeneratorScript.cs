dea45245bf925b0fd66f69b9acb3f7e71c7e07f5
Giau Ngo <giaunv@gmail.com>
2015-04-15 06:58:12 +0700

    Generating Endless Level: The Method to Add New Room

--- Assets/Scripts/GeneratorScript.cs
+++ Assets/Scripts/GeneratorScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GeneratorScript : MonoBehaviour {
 	public GameObject[] availableRooms;
@@ -8,12 +9,26 @@ public class GeneratorScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		float height = 2.0f + Camera.main.orthographicSize;
-		screenWidthInPoints = height + Camera.main.aspect;
+		float height = 2.0f * Camera.main.orthographicSize;
+		screenWidthInPoints = height * Camera.main.aspect;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void AddRoom(float farhtestRoomEndX){
+		int randomRoomIndex = Random.Range(0, availableRooms.Length);
+
+		GameObject room = (GameObject)Instantiate(availableRooms[randomRoomIndex]);
+
+		float roomWidth = room.transform.FindChild("Floor").localScale.x;
+
+		float roomCenter = farhtestRoomEndX + roomWidth * 0.5f;
+
+		room.transform.position = new Vector3(roomCenter, 0, 0);
+
+		currentRooms.Add(room);
 	}
 }
